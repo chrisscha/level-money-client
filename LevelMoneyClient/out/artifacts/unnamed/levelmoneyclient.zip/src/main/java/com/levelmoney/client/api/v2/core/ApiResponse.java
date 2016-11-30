e1a8543f5eca612ac936bbe2585ee6a3a38600f3
chrisscha <cschattauer@gmail.com>
2016-11-30 02:09:03 -0800

    Remove more accidentally committed files

--- LevelMoneyClient/out/artifacts/unnamed/levelmoneyclient.zip/src/main/java/com/levelmoney/client/api/v2/core/ApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.levelmoney.client.api.v2.core;
-
-/**
- * Marker interface to make sure you actually return a response object from the service
- * <p>
- * Created by chris on 11/27/16.
- */
-public interface ApiResponse {
-}
